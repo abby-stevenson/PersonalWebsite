868a939c837cf435a4aee489fa25fcd952a1c804
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-05-02 19:03:55 +0100

    About Me Section

--- app/src/AboutMe.jsx
+++ app/src/AboutMe.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import './AboutMe.css';
+import HeadShot from './assets/f24-headshots-7-copy.jpeg';
 
 export default function AboutMe() {
-    return (
-        <div className = "aboutme">
-            <h2>About Me</h2>
-            <div>
-                <div>
-
-                </div>
-                <div>
-
-                </div>
+  return (
+    <section className="aboutme">
+      <h2>About Me</h2>
+      <div className="aboutme-border">
+        <div className="image-and-text">
+            <div className="left-hand-side">
+            <img className="headshot" src={HeadShot} alt="Abby Stevenson Headshot" />
             </div>
-
+            <div className="right-hand-side">
+            <p>
+                Hello! I'm a second-year Computer Science student at Northeastern University, concentrating in Artificial Intelligence and minoring in Mathematics.
+                I'm excited to be starting my first co-op at Toast in the coming months.
+            </p>
+            <p>
+                I grew up in a small town just north of London, and since starting at Northeastern in Fall 2023, I’ve been based in Boston.
+            </p>
+            <p>
+                Last semester I took Intro to Database Design, Computer Systems, Probability and Statistics and Theory of Computation. 
+            </p>
+            </div>
         </div>
-
-    );
-};      
+      </div>
+    </section>
+  );
+}
