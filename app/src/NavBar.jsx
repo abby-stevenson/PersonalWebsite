5f028c78e778435cd2b780ffcf121e85f7611a53
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-05-03 18:36:53 +0100

    Cleaning up Nav Bar

--- app/src/NavBar.jsx
+++ app/src/NavBar.jsx
@@ -16,7 +16,6 @@ import './NavBar.css';
                     <button className="nav-button">Education</button>
                 </a>
                 <button className='nav-button'>Projects</button>
-                <button className='nav-button'>Extracurriculars</button>
                 <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer">
                     <button className="nav-button">Resume</button>
                 </a>
