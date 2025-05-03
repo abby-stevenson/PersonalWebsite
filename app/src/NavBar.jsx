87726011c51f0a7867847e614e87c127eb5741c1
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-05-03 16:32:29 +0100

    Adding a link to the education section

--- app/src/NavBar.jsx
+++ app/src/NavBar.jsx
@@ -12,18 +12,20 @@ import './NavBar.css';
         </div>
         <div className="navbar-right">
             <div className="nav-links">
-                    <button className='nav-button'>Education</button>
-                    <button className='nav-button'>Projects</button>
-                    <button className='nav-button'>Extracurriculars</button>
-                    <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer">
-                        <button className="nav-button">Resume</button>
-                    </a>
-                    <a href="https://www.linkedin.com/in/abbyostevenson/" target="_blank" rel="noopener noreferrer">
-                        <button className='nav-button'>LinkedIn</button>
-                    </a>
-                    <a href="https://github.com/abby-stevenson" target="_blank" rel="noopener noreferrer">
-                        <button className='nav-button'>Github</button>
-                    </a>
+                <a href="#education">
+                    <button className="nav-button">Education</button>
+                </a>
+                <button className='nav-button'>Projects</button>
+                <button className='nav-button'>Extracurriculars</button>
+                <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer">
+                    <button className="nav-button">Resume</button>
+                </a>
+                <a href="https://www.linkedin.com/in/abbyostevenson/" target="_blank" rel="noopener noreferrer">
+                    <button className='nav-button'>LinkedIn</button>
+                </a>
+                <a href="https://github.com/abby-stevenson" target="_blank" rel="noopener noreferrer">
+                    <button className='nav-button'>Github</button>
+                </a>
             </div>
         </div>
     </nav>
