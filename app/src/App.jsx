c829d89f61f720b889167b79266a7e12001eb20d
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-05-03 17:20:40 +0100

    Removing unnecessary router

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './NavBar';
 import AboutMe from './AboutMe';
 import Education from './Education';
+
 
 /* Sets up the main page and set up the routes to the other pages so that the use can interact with the 
 buttons and it will take then to the appropriate page */
 export default function App() {
   return (
-      <Router>
-      
-          <Navbar />
-          <AboutMe />
-          <Education />
-          
-          <Routes>
-            {/** 
-            <Route path="/" element={<YourRecommendations />} />
-            <Route path="/recommendations" element={<YourRecommendations />} />
-            <Route path="/continue-watching" element={<ContinueWatching />} />
-            */}
-          </Routes>
-        
-      </Router>
+      <div>
+        <Navbar />
+        <AboutMe />
+        <Education />
+      </div> 
     );
 }
