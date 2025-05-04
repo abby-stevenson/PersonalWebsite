46448b59bb1c9c7cf6e1cc527da5f8bad670e7e9
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-05-04 15:44:23 +0100

    Adding Project Link to Nav Bar

--- app/src/NavBar.jsx
+++ app/src/NavBar.jsx
@@ -15,8 +15,10 @@ import './NavBar.css';
                 <a href="#education">
                     <button className="nav-button">Education</button>
                 </a>
-                <button className='nav-button'>Projects</button>
-                <button className='nav-button'>Work Experience</button>
+                <a href="#projects">
+                    <button className='nav-button'>Projects</button>
+                </a>
+                <button className='nav-button'>Experience</button>
                 <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer">
                     <button className="nav-button">Resume</button>
                 </a>
