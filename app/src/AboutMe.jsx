a3d863a221085532caab44a59a23443f91aaa363
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-07-03 14:21:16 +0100

    About me changes

--- app/src/AboutMe.jsx
+++ app/src/AboutMe.jsx
@@ -13,13 +13,13 @@ export default function AboutMe() {
             </div>
             <div className="right-hand-side">
             <p>
-                Hello! I'm a second-year Computer Science student at Northeastern University, concentrating in Artificial Intelligence and minoring in Mathematics.
+                Hello! I'm a second-year Computer Science student at Northeastern University, with a concentration in Artificial Intelligence and minoring in Mathematics.
                 I'm excited to be starting my first co-op at Toast in the coming months.
             </p>
             <p>
                 I grew up in a small town just north of London, and since starting at Northeastern in Fall 2023, I’ve been based in Boston.
             </p>
-            <p> I love Taylor Swift's Music, I am also super interested in the Theme Park Industy specifically the intersection between Computer Science and this industy.
+            <p> I am super interested in the Theme Park Industy specifically the intersection between Computer Science and this industy.
               I am also passionate about Computer Science in Education and getting young kids involved in all STEM subjects.
             </p>
             </div>
