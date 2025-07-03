76a3b33449e58d40f6d1cdb4bbb6679b03739162
abby-stevenson <147872370+abby-stevenson@users.noreply.github.com>
2025-07-03 15:16:09 +0100

    Fixing Resume Link

--- app/src/NavBar.jsx
+++ app/src/NavBar.jsx
@@ -18,7 +18,7 @@ import './NavBar.css';
                 <a href="#projects">
                     <button className='nav-button'>Projects</button>
                 </a>
-                <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer">
+                <a href="Resume.pdf" target="_blank" rel="noopener noreferrer">
                     <button className="nav-button">Resume</button>
                 </a>
                 <a href="https://www.linkedin.com/in/abbyostevenson/" target="_blank" rel="noopener noreferrer">
